0199c887141e23eb3f2ff3c19a2b483ad3dc0f9c
Gauhar2003 <gauhariftekhar95@gmail.com>
2026-01-12 15:37:47 +0530

    ai personal assistent added

--- config/env.js
+++ config/env.js
@@ -2,6 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT || 5000;
-export const MONGO_URI = process.env.MONGODB_ATLAS_URI;
+export const MONGO_URI = process.env.MONGODB_ATLAS_URI || process.env.MONGODB_URI;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const NODE_ENV = process.env.NODE_ENV || 'development';
